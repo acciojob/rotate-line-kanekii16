993a8127a2ccc9e86bc9240a1ff5fc47b8157080
kanekii16 <96989943+kanekii16@users.noreply.github.com>
2023-09-28 13:22:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const  line_box = document.getElementById("line");
+
+window.onload(append());
+
+function append(){
+    console.log("functio triggred");
+    const div = document.createElement("div");
+    div.className="lineDiv"
+    line_box.appendChild(div);
+}
